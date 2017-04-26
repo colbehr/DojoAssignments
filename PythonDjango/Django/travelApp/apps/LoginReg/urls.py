8a46bcd144fa77e95379da8f6283319d7da3d7e2
colbehr <colbehr@gmail.com>
2017-04-26 12:34:28 -0700

    Deleteing App

--- PythonDjango/Django/travelApp/apps/LoginReg/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-urlpatterns = [
-	url(r'^$', views.index, name= 'index'),
-	url(r'^login/$', views.processLogin, name = "login"),
-	url(r'^register/$', views.processReg, name = "reg"),
-	url(r'^logout/$', views.logout, name = "logout"),
-]
